bc983398d81eef277b4881e8b5d09669066bbd38
Ashwani <ashwanichoudhary25@gmail.com>
2023-07-26 15:25:14 +0530

    Initial Commit

---
+++ GitIgnoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitIgnoreApp
+//
+//  Created by Ashwani Choudhary on 26/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
